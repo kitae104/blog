b25c6db6926636c7dad1f977cb5052e2dcf72733
Kitae's Github <aqua0405@gmail.com>
2023-03-29 02:07:16 +0900

    Update BlogApplication.java

--- src/main/java/kr/inhatc/blog/BlogApplication.java
+++ src/main/java/kr/inhatc/blog/BlogApplication.java
@@ -2,7 +2,9 @@ package kr.inhatc.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing            // 데이터 변동이 있으면 JPA가 반영해줌
 @SpringBootApplication
 public class BlogApplication {
 
