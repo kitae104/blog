2177d1b29078eca87fe9cdfb9ee42251fa371049
Kitae's Github <aqua0405@gmail.com>
2023-04-02 02:21:49 +0900

    Update Reply.java

--- src/main/java/kr/inhatc/blog/board/entity/Reply.java
+++ src/main/java/kr/inhatc/blog/board/entity/Reply.java
@@ -1,13 +1,6 @@
 package kr.inhatc.blog.board.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import kr.inhatc.blog.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
