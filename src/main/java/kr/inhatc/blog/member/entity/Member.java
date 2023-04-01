efdf26777c73b38dae45b3c36f882b5c52465af2
Kitae's Github <aqua0405@gmail.com>
2023-04-02 00:33:14 +0900

    Update Member.java

--- src/main/java/kr/inhatc/blog/member/entity/Member.java
+++ src/main/java/kr/inhatc/blog/member/entity/Member.java
@@ -2,6 +2,7 @@ package kr.inhatc.blog.member.entity;
 
 
 
+import kr.inhatc.blog.member.dto.MemberFormDto;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
@@ -35,7 +36,7 @@ public class Member extends BaseEntity
 	private Long id;
 	
 	@Column(nullable = false, length = 30)
-	private String username;
+	private String name;
 	
 	@Column(nullable = false, length = 100)
 	private String password;
@@ -43,8 +44,25 @@ public class Member extends BaseEntity
 	@Column(nullable = false, length = 50)
 	private String email;
 
+	private String address;
 	
 	@Enumerated(EnumType.STRING)  // 문자열 형태로 사용
 	@Column(nullable = false, length = 20, columnDefinition = "varchar(20) default 'USER'")	
-    private Role role; 
+    private Role role;
+
+	/**
+	 *
+	 * @param memberFormDto 회원가입 폼
+	 * @return 회원가입 폼을 Member 객체로 변환
+	 */
+	public static Member createMember(MemberFormDto memberFormDto){
+		Member member = new Member();
+		member.setName(memberFormDto.getName());
+		member.setEmail(memberFormDto.getEmail());
+		member.setAddress(memberFormDto.getAddress());
+		//String password = passwordEncoder.encode(memberFormDto.getPassword());  // 비밀 번호 암호화
+		member.setPassword(memberFormDto.getPassword());
+		member.setRole(Role.USER);
+		return member;
+	}
 }
