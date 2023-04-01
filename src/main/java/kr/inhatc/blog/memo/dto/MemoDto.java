850b34c17b5b8efd02c638e151c374eff3d1a6e7
Kitae's Github <aqua0405@gmail.com>
2023-04-02 02:22:27 +0900

    Update MemoDto.java

--- src/main/java/kr/inhatc/blog/memo/dto/MemoDto.java
+++ src/main/java/kr/inhatc/blog/memo/dto/MemoDto.java
@@ -5,13 +5,18 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @ToString
 public class MemoDto {
 
+    private Long id;
     @NotBlank(message = "사용자명은 필수 입력 값입니다.")
     private String userName;
     
     @NotNull(message = "내용 입력은 필수 입력 값입니다.")
     private String contents;
+
+    private LocalDateTime modifiedBy;
 }
