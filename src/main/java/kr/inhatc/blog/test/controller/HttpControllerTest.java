fa102c7f576c4e6b79b1570a88c0d2296c970c59
Kitae's Github <aqua0405@gmail.com>
2023-04-02 02:22:38 +0900

    Update HttpControllerTest.java

--- src/main/java/kr/inhatc/blog/test/controller/HttpControllerTest.java
+++ src/main/java/kr/inhatc/blog/test/controller/HttpControllerTest.java
@@ -1,15 +1,8 @@
 package kr.inhatc.blog.test.controller;
 
 
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import kr.inhatc.blog.test.entity.Member;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HttpControllerTest
