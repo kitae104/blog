4432f239508f146b3fe30b4028371ca687d7ac4b
Kitae's Github <aqua0405@gmail.com>
2023-04-02 02:22:01 +0900

    Update MemberRepository.java

--- src/main/java/kr/inhatc/blog/member/repository/MemberRepository.java
+++ src/main/java/kr/inhatc/blog/member/repository/MemberRepository.java
@@ -1,11 +1,8 @@
 package kr.inhatc.blog.member.repository;
 
+import kr.inhatc.blog.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import kr.inhatc.blog.member.entity.Member;
-
-import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>
