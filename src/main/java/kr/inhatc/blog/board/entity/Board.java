98dd3104d31713a2152dc0842cdd8a7372ab6c8d
Kitae's Github <aqua0405@gmail.com>
2023-04-02 02:21:30 +0900

    Update Board.java

--- src/main/java/kr/inhatc/blog/board/entity/Board.java
+++ src/main/java/kr/inhatc/blog/board/entity/Board.java
@@ -1,25 +1,15 @@
 package kr.inhatc.blog.board.entity;
 
-import java.util.List;
-
-import org.hibernate.annotations.ColumnDefault;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import kr.inhatc.blog.member.entity.Member;
 import kr.inhatc.blog.utils.audit.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
