393aa6d4789c72b9c7c4383ccb92c75046571f1d
Kitae's Github <aqua0405@gmail.com>
2023-04-03 00:18:00 +0900

    Update GlobalExceptionHandler.java

--- src/main/java/kr/inhatc/blog/utils/handler/GlobalExceptionHandler.java
+++ src/main/java/kr/inhatc/blog/utils/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package kr.inhatc.blog.utils.handler;
 
 import jakarta.persistence.EntityNotFoundException;
+import kr.inhatc.blog.utils.common.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    public String handleArgumentException(IllegalArgumentException e) {
-        return "<h1>" + e.getMessage() + "<h1>";
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 
-    @ExceptionHandler(value = EntityNotFoundException.class)
-    public String handleEntityNotFoundException(EntityNotFoundException e) {
-        return "<h1>" + e.getMessage() + "<h1>";
-    }
+//    @ExceptionHandler(value = IllegalArgumentException.class)
+//    public String handleArgumentException(IllegalArgumentException e) {
+//        return "<h1>" + e.getMessage() + "<h1>";
+//    }
+//
+//    @ExceptionHandler(value = EntityNotFoundException.class)
+//    public String handleEntityNotFoundException(EntityNotFoundException e) {
+//        return "<h1>" + e.getMessage() + "<h1>";
+//    }
 }
