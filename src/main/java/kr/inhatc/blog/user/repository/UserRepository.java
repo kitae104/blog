96e0bb6d4a4537689a56018b9a2dbdade6bf8f8c
Kitae's Github <aqua0405@gmail.com>
2023-04-04 21:54:15 +0900

    Update UserRepository.java

--- src/main/java/kr/inhatc/blog/user/repository/UserRepository.java
+++ src/main/java/kr/inhatc/blog/user/repository/UserRepository.java
@@ -2,6 +2,12 @@ package kr.inhatc.blog.user.repository;
 
 import kr.inhatc.blog.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmailAndPassword(String email, String password);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1 AND u.password = ?2")
+    User login(String email, String password);
 }
